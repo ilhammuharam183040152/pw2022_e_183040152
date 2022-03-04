f2a861bceb956647580f46b34ac557fab22a52d8
ilhammuharam183040152 <ilham.sn29@gmail.com>
2022-03-04 10:59:56 +0700

    first commit

---
+++ pertemuan2/index.php
@@ -0,0 +1,54 @@
+<?php
+    // pertemuan2 
+    // sintaks dasar
+
+    // php di dalam html
+    // gtml di dalam php
+
+    //variabel dan type data
+    //variabel
+    //tidak boleh di awali dengan angka, tapi boleh mengandung angka
+    // $nama = "ilham" ;
+    // echo "hallo, nama saya, $nama" ;
+
+    // operator
+    // aritmatika
+    // + - * / %
+
+    // $x = 20 ;
+    // $y = 50 ;
+    // echo $x * $y ; 
+
+    //penggabung string / concatenation / concat
+    // .
+
+    // $nama_depan = "ilhham" ;
+    // $nama_belakang = "muharam" ;
+    // echo $nama_depan . " " . $nama_belakang ;
+
+    // assigment 
+    // =, +=, -=, *=, /=, %=, .=
+
+    // $x = 1;
+    // $x -= 5;
+    // echo $x;   
+    // $nama = "ilham" ;
+    // $nama .= " ";
+    // $nama .= "muharam";
+    // echo $nama ;
+
+    //perbandingan
+    // <, >, <=, >=, ==, !=
+    // var_dump(1 == "1");
+
+    //identitas
+    // ===, !==
+    // var_dump(1 === "1");
+
+    //logika
+    // &&, ||, !
+    $x = 30 ;
+    var_dump($x < 20 || $x % 2 == 0);
+
+
+?>
---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,49 @@
+<?php
+// pengulanan
+//for
+//while
+// do.. while
+//foreach : pengulangan khusus array
+
+// for( $i = 0; $i < 5; $i++ ) {
+//     echo "hello world <br>";
+// }
+
+// $i = 0;
+// while ($i < 5) {
+//     echo "hello world <br>";
+//     $i++;
+// }
+// $i = 0;
+// do {
+//     echo " hello world <br>";
+//     $i++;
+// } while ($i < 10);
+?>
+<html>
+
+    <head>
+        <title>latihan1</title>
+        <style>
+            .warna-baris {
+                background-color : silver ;
+            }
+        </style>
+    </head>
+    <body>
+        
+    <table border="1" cellpadding="10" cellspacing="0">
+        <?php for ( $i =1; $i <=5; $i++) : ?>
+            <?php if( $i % 2 == 1) : ?>
+                <tr class="warna-baris">
+            <?php else : ?>
+                <tr>
+            <?php endif; ?>
+                <?php for( $j = 1; $j <= 5; $j++ ) : ?>
+                    <td><?= "$i, $j"; ?></td>
+                <?php endfor; ?>
+            </tr>
+        <?php endfor; ?>
+    </table>
+    </body>
+</html>
